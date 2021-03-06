d8fff31c3b18401d10841c8c30d084ceadd40420
Mattia <matpolli93@gmail.com>
2021-03-06 12:11:32 +0100

    Added replay button feature

--- js/script.js
+++ js/script.js
@@ -6,47 +6,46 @@ let highscore = 0;
 
 // Adding "click" event
 document.querySelector(".check").addEventListener("click",
-    function() {
+function() {
     const guess = Number(document.querySelector(".guess").value);
     
     // When there is no input
     if (!guess) {
         document.querySelector(".message").textContent = "No number!";
-
+        
         // When player wins
     } else if (guess === secretNumber) {
-        document.querySelector(".message").textContent = "Well done, you got the Correct Number!";
+        document.querySelector(".message").textContent = "Well done, you got the Correct Number! 🎉";
         document.querySelector(".number").textContent = secretNumber;
         
         // Background-color and "Correct Number" properties that are displayed when player wins
         document.querySelector("body").className = "bg-green";        
-        document.querySelector(".number").className = "width";
-        document.querySelector(".message").className = "mg-bottom";
+        document.querySelector(".message").className = "properties";
         
         // Highscore feature
         if (score > highscore) {
             highscore = score;
             document.querySelector(".highscore").textContent = highscore;
         }
-
+        
         // When guess is too high
     } else if (guess > secretNumber) {
         if (score > 1) {
             document.querySelector(".message").textContent = "Oops, too high!";
             score--;
             document.querySelector(".score").textContent = score;
-        }   else {
+        } else {
             document.querySelector(".message").textContent = "Nooo you lost the game!";
             document.querySelector(".score").textContent = 0;
         }
-
+        
         // When guess is too low
     } else if (guess < secretNumber) {
         if (score > 1) {
             document.querySelector(".message").textContent = "Nope, too low!";
             score--;
             document.querySelector(".score").textContent = score;
-        }   else {
+        } else {
             document.querySelector(".message").textContent = "Nooo you lost the game!";
             document.querySelector(".score").textContent = 0;
         }
@@ -54,3 +53,20 @@ document.querySelector(".check").addEventListener("click",
 }
 );
 
+// Replay button feature
+document.querySelector(".again").addEventListener("click",
+    function() {
+    score = 20;
+    secretNumber = Math.trunc(Math.random()*20) + 1;
+    
+    // Reset features
+    document.querySelector(".message").textContent = "Start guessing again...";
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".number").textContent = "?";
+    document.querySelector(".guess").value = "";
+
+    // Style features
+    // document.querySelector("body").className = "bg-grey";
+    // document.querySelector(".number").className = "width";
+    }
+);
